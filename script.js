f0958477f8c4faa1ab26de1decb285f9a1f59abe
mohdsuhaill <suhailabdmohammed@gmail.com>
2024-06-12 16:03:35 +0530

    update

---
+++ script.js
@@ -0,0 +1,37 @@
+// using function create dom elements
+function element(tag, classname, id, text) {
+  let tags = document.createElement(tag);
+  tags.classList = classname;
+  tags.id = id;
+  tags.innerHTML = text;
+  return tags;
+}
+
+// create a (container ,heading , row)
+let container = element("div", "container", "", "");
+const h1 = element("h1", "text-center", "title", "MEMES BOARD");
+const row = element("div", "row", "", "");
+
+// getting Memes api
+const response = fetch("https://api.imgflip.com/get_memes");
+response
+  .then((data) => data.json())
+  .then((result) => {
+    for (let i = 0; i < result.data.memes.length; i++) {
+      const col = document.createElement("div");
+      col.classList = "col-sm-5 col-md-3 col-lg-4 col-xl-4";
+      col.innerHTML = `
+      <div class="card" style="width: 18erm;">
+      <img src="${result.data.memes[i].url}" alt="memes img">
+       <div class ="card-body">
+        <h4 class=text-center>${result.data.memes[i].name}</h4>
+       </div>
+      </div>
+      `
+      row.append(col)
+    }
+  });
+
+// appending part
+document.body.append(h1, container);
+container.append(row);
